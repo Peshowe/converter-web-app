0b3417a3430f4a8ab50feaa97565f6a204ce0405
root <nedelevbg@gmail.com>
2021-02-14 13:24:40 +0100

    certbot container setup

--- start-server.sh
+++ start-server.sh
@@ -4,4 +4,4 @@ if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; 
     (cd converter_web_app; python manage.py createsuperuser --no-input)
 fi
 (cd converter_web_app; gunicorn converter_web_app.wsgi --user www-data --bind 0.0.0.0:8010 --workers=1 --threads=2 --worker-class=gthread --timeout 600 --log-level debug) &
-while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g "daemon off;"
+(while :; do sleep 6h & wait ${!}; nginx -s reload; done & nginx -g "daemon off;")

8a1484a16b44afe27a3a1dc797e9d67368285aab
root <nedelevbg@gmail.com>
2021-02-14 12:05:17 +0100

    no-ssl

--- converter_web_app/settings.py
+++ converter_web_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "converter.cfapps.io", "116.203.239.29"]
+ALLOWED_HOSTS = ["127.0.0.1", "converter.cfapps.io", "116.203.239.29", "pravopis.osvedomitel.bg"]
 
 APPEND_SLASH = False
 # Application definition
